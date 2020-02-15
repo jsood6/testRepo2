207fcb7dfffbd2d64c4decb7bbf508dc71a12e15
Jigyasaa Sood <jigyasaasood@Jigyasaas-MacBook-Pro.local>
2020-02-15 13:36:43 -0800

    Initial Commit

---
+++ testProj2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProj2
+//
+//  Created by Jigyasaa Sood on 2/15/20.
+//  Copyright © 2020 Jigyasaa Sood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
