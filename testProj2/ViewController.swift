d87903da02bf9d8239207ae1d7e6a17c3424d327
Jigyasaa Sood <jigyasaasood@Jigyasaas-MacBook-Pro.local>
2020-02-15 13:49:01 -0800

    committing with Xcode!!!!

--- testProj2/ViewController.swift
+++ testProj2/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         //edit blah comment......
     }
+    
+    func random() {
+        //blah blah
+        
+    }
 
 
 }
